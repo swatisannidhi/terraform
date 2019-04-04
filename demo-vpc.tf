92d76901803d1b8958fafcab04aa8b20e42573d0
Sannidhi <Swati.Sannidhi@savvis.com>
2019-04-04 09:05:35 +0800

    test commit

---
+++ demo-vpc.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "demo-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+
+resource "aws_subnet" "demo-public-subnet" {
+  vpc_id  = "${aws_vpc.demo-vpc.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "demo-public-subnet"
+  }
+}
+
+resource "aws_subnet" "demo-private-subnet" {
+  vpc_id  = "${aws_vpc.demo-vpc.id}"
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "demo-public-subnet"
+  }
+}
