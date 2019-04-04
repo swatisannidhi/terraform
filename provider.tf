b04da3e27229f0e14e4f1dd8c31df1a569c368ff
Sannidhi <Swati.Sannidhi@savvis.com>
2019-04-04 08:59:41 +0800

    commit provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,4 @@
-provider "aws" {
+provider "aws"
+{
   region = "ap-southeast-1"
-}
-
-resource "aws_instance" "tf-web" {
-  ami           = "ami-76144b0a"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
